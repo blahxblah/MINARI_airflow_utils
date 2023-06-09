c3e91403cb76537cf5af42cfa429a46528e8fa8c
blahxblah <id.blahxblah@gmail.com>
2023-06-09 15:59:11 +0900

    0.5.0

--- src/MINARI_airflow_utils_pip/MINARI_airflow_utils_pip.py
+++ src/MINARI_airflow_utils_pip/MINARI_airflow_utils_pip.py
@@ -13,7 +13,7 @@ Example:
 
 from airflow.operators.bash import BashOperator
 
-def gen_bash_task(name: str, cmd: str, dag, trigger="all_success"):
+def gen_bash_task(name: str, cmd: str, test_dag, trigger="all_success"):
     """airflow bash task 생성
         - trigger-rules : https://airflow.apache.org/docs/apache-airflow/stable/core-concepts/dags.html#trigger-rules
     """
@@ -21,6 +21,6 @@ def gen_bash_task(name: str, cmd: str, dag, trigger="all_success"):
         task_id=name,
         bash_command=cmd,
         trigger_rule=trigger,
-        dag=dag
+        dag=test_dag
     )
     return bash_task
