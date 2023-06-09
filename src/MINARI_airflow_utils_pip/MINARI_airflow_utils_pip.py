a5ba965db090acff0c85750075f342a7d9ae67e6
blahxblah <id.blahxblah@gmail.com>
2023-06-09 14:37:42 +0900

    .

--- src/MINARI_airflow_utils_pip/MINARI_airflow_utils_pip.py
+++ src/MINARI_airflow_utils_pip/MINARI_airflow_utils_pip.py
@@ -6,36 +6,21 @@ Example:
     ``MINARI_airflow_utils_pip`` 사용법은 아래와 같습니다.
 
         $ pip install ./
-        $ MINARI_airflow_utils_pip-ping
 
 추가적인 설명은 여기에!
 
-Attributes:
-    nnn (int): ``사용되지 않는`` 시범용 변수
-
-Todo:
-    * 무한한 모듈의 발전 ``꿈``꾸며!
-    * ``Dreaming`` of infinite module development!
-
 """
-import sys
 
-nnn = 1
+from airflow.operators.bash import BashOperator
 
-
-def ping():
-    """Example function with PEP 484 type annotations.
-
-    Returns:
-        리턴 없이 화면 print
-
+def gen_bash_task(name: str, cmd: str, dag, trigger="all_success"):
+    """airflow bash task 생성
+        - trigger-rules : https://airflow.apache.org/docs/apache-airflow/stable/core-concepts/dags.html#trigger-rules
     """
-    if len(sys.argv) > 1:
-        n = int(sys.argv[1])
-        print(f"{'p' * n}ong")
-    else:
-        print('pong')
-
-
-if __name__ == "__main__":
-    ping()
+    bash_task = BashOperator(
+        task_id=name,
+        bash_command=cmd,
+        trigger_rule=trigger,
+        dag=dag
+    )
+    return bash_task
